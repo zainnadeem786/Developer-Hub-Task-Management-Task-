f4443dae0c08fc5e078c29ae88d155f4f49ccd0c
zainnadeem786 <zainnadeemzainnadeem80@gmail.com>
2024-12-26 15:27:35 +0500

    project start

--- tasks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('tasks/', views.TaskListView.as_view(), name='task-list'),
-    path('tasks/<int:pk>/', views.TaskDetailView.as_view(), name='task-detail'),
-]
